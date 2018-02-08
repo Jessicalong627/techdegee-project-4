5ce4ce34014858697273cc1f2e7413e33a1d484c
Jessica Long <jessicalong627@gmail.com>
2018-02-08 15:13:17 -0500

    photo changes

--- js/main.js
+++ js/main.js
@@ -1,29 +1,27 @@
-/*$(document).ready(function() {
-  $('.gallery').magnificPopup({
-  type: 'image',
-  gallery:{
-    enabled:true
-  }
-});/*
-	$('.gallery').magnificPopup({
+
+$(document).ready(function() {
+	$('.layout').magnificPopup({
 		delegate: 'a',
 		type: 'image',
 		tLoading: 'Loading image #%curr%...',
 		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,6] // Will preload 0 - before current, and 1 after the current image
+    gallery: {
+  enabled: true, // set to true to enable gallery
+
+  preload: [1,4], // read about this option in next Lazy-loading section
+
+  navigateByImgClick: true,
+
   arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
 
   tPrev: 'Previous (Left arrow key)', // title for left button
   tNext: 'Next (Right arrow key)', // title for right button
-  tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of coun
-		},
+  tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+},
 		image: {
 			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
 			titleSrc: function(item) {
-				return item.el.attr('title') + '<small>Jess</small>';
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
 			}
 		}
 	});
