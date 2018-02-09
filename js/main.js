d1a0960591f5665d68b5fb9ca889d02cbf859590
Jessica Long <jessicalong627@gmail.com>
2018-02-09 15:00:17 -0500

    search bar

--- js/main.js
+++ js/main.js
@@ -1,28 +1,6 @@
-
-$(document).ready(function() {
-	$('.layout').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-    gallery: {
-  enabled: true, // set to true to enable gallery
-
-  preload: [1,4], // read about this option in next Lazy-loading section
-
-  navigateByImgClick: true,
-
-  arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
-
-  tPrev: 'Previous (Left arrow key)', // title for left button
-  tNext: 'Next (Right arrow key)', // title for right button
-  tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
-},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-			}
-		}
+jQuery(function($) {
+	$('#layout').lookingfor({
+		input: $('input[name="query"]'),
+		items: 'li'
 	});
 });
